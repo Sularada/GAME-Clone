0ec8eca471ab4f86bb4faec38b970ad3965f3d37
Sularada <111848753+Sularada@users.noreply.github.com>
2025-07-01 14:43:58 +0300

    gift rework bug fixed

--- game_plus_clone/src/stories/components/molecules/Gift/Gift.jsx
+++ game_plus_clone/src/stories/components/molecules/Gift/Gift.jsx
@@ -1,13 +1,12 @@
 import "./style.scss";
 import { Col, Row } from "react-bootstrap";
-import { LinkButton } from "../../atoms/Button/Button";
 import H2 from "../../atoms/H2/H2";
 import P from "../../atoms/P/P";
 import Link from "../../atoms/Link/Link";
 
 const Gift = ({ data, line = "" }) => {
   return (
-    <div className="gift positon-relative w-100 d-flex justify-content-center">
+    <div className="gift position-relative w-100 d-flex justify-content-center">
       <Row className="gift-container justify-content-center w-100">
         <Col md={8} lg={6}>
           <H2 text={data.title} classes={"text-primary " + line} />
---
+++ game_plus_clone/src/stories/components/molecules/InfoCard/InfoCard.stories.jsx
@@ -0,0 +1,21 @@
+import InfoCard from "./InfoCard";
+
+const meta = {
+  component: InfoCard,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    info: {
+      top_icon_class: "bi bi-controller",
+      title: "Download and Play",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, autem.",
+      link: "#",
+      link_text: "Download Now",
+      link_icon_class: "bi bi-download",
+    },
+  },
+};
