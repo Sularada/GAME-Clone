d52f8fb7c4fce0694c6156d7eb2fa6b2ea08af1d
Sularada <111848753+Sularada@users.noreply.github.com>
2025-07-01 10:56:00 +0300

    game card rework

--- game_plus_clone/src/stories/components/molecules/GameCard/GameCard.jsx
+++ game_plus_clone/src/stories/components/molecules/GameCard/GameCard.jsx
@@ -1,11 +1,23 @@
 "use client";
 import { Card } from "react-bootstrap";
 import NextImage from "../../atoms/Img/NextImage";
-
+import "./style.scss";
+import Link from "../../atoms/Link/Link";
 const GameCard = ({ game }) => {
   return (
-    <Card style={{ width: "10rem" }} bg="dark">
-      <Card.Img variant="top" src={game.img} />
+    <Card className="game-card" bg="dark">
+      <div className="game-card-top position-relative">
+        <div className="card-img">
+          <Card.Img variant="top" src={game.img} />
+        </div>
+        <Link
+          href={game.link}
+          classes="position-absolute btn btn-primary rounded-0 text-white fw-bolder"
+          text="OYNAT"
+          rightIcon="https://gameplus.com.tr/static/media/icon-play.914b7f60.webp"
+        />
+      </div>
+
       <Card.Body>
         <Card.Title className="text-white fs-6">{game.name}</Card.Title>
         <Card.Text className="text-light" style={{ fontSize: "14px" }}>
---
+++ game_plus_clone/src/stories/components/molecules/GameCard/GameCard.stories.jsx
@@ -0,0 +1,22 @@
+import GameCard from "./GameCard";
+
+const meta = {
+  component: GameCard,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    game: {
+      name: "Game Name",
+      img: "https://img.nvidiagrid.net/apps/100884811/ZZ/KEY_ART_01_511b71f8-31e3-49fc-9b92-3d22e51f2ea3.jpg",
+      link: "",
+      company: "Company Name",
+      platform: "Platform Name",
+      platform_img:
+        "	https://gameplus.com.tr/static/media/store-steam-logo.b62069c5.webp",
+    },
+  },
+};
